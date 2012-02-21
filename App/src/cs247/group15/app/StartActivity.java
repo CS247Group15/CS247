54e37f31b6012c91fdf6e639f4af89e11e672719
kbarrett <x_kimmi_13_x@hotmail.co.uk>
2012-02-21 22:03:57 +0000

    Created some basic folder structures

---
+++ App/src/cs247/group15/app/StartActivity.java
@@ -0,0 +1,13 @@
+package cs247.group15.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
