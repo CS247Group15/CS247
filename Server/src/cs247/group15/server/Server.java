abc8b762baa65a6ba8836b7c7e8fe95b13079eef
kbarrett <x_kimmi_13_x@hotmail.co.uk>
2012-04-30 17:59:38 +0100

    rearranged folders

---
+++ Server/src/cs247/group15/server/Server.java
@@ -0,0 +1,36 @@
+package cs247.group15.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import cs247.group15.data.Constants;
+
+public class Server {
+	
+	public static void main(String[] args)
+	{
+		try {
+			ServerSocket serverSocket = new ServerSocket(Constants.serverPort);
+			System.out.println("Server has started.");
+			while (true)
+			{
+				try
+				{
+					Socket clientSocket = serverSocket.accept();
+					new DataSender(clientSocket).start();
+					clientSocket.close();
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Server/src/cs247/group15/server/webBot.java
@@ -0,0 +1,33 @@
+package cs247.group15.server;
+import java.net.URL;
+import java.io.*;
+import java.net.*;
+
+public class webBot {
+	
+	public static void main(String[] args) {
+		
+		
+		String bbcUrl = "http://feeds.bbci.co.uk/news/rss.xml";
+		String bbcSourceDescription = "BBC News RSS Feed";
+		String guardianUrl = "http://feeds.guardian.co.uk/theguardian/world/rss";
+		String guardianSourceDescription = "The Guardian RSS Feed";
+		
+		
+		rssReader reader = new rssReader(bbcUrl, bbcSourceDescription);
+		reader.start();
+		
+		rssReader guardianReader = new rssReader(guardianUrl, guardianSourceDescription);
+		guardianReader.start();
+		
+		
+		googleTrendsReader google = new googleTrendsReader();
+		google.start();
+		
+	}
+	
+
+	
+}
+	
+
