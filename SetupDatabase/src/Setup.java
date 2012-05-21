674e5b4d000409d4c7b48b068b69b76d7aafd7e3
SebCompski <gate_tremor@msn.com>
2012-05-21 22:00:28 +0100

    Updated the importance and semantics list, still not enough words to really give a good result though. Also added a bit of code to deal with punctuation after words being added to the nouns list.

--- SetupDatabase/src/Setup.java
+++ SetupDatabase/src/Setup.java
@@ -121,10 +121,10 @@ public class Setup {
 
 		String createString;
 		createString = "create table rawdata (" +
-							"Title VARCHAR(300), " +
+							"Title VARCHAR(300) NOT NULL, " +
 							"Description VARCHAR(500), " +
 							"Source VARCHAR(500), " +
-							"Date DATE)";
+							"Date DATE);";
 		try {
 			create = con.createStatement();
 			create.executeUpdate(createString);
@@ -144,8 +144,8 @@ public class Setup {
 
 		String createString;
 		createString = "create table Words (" +
-							"Word VARCHAR(30), " +
-							"Value INTEGER)";
+							"Word VARCHAR(30) NOT NULL, " +
+							"Value INTEGER);";
 		try {
 			create = con.createStatement();
 			create.executeUpdate(createString);
